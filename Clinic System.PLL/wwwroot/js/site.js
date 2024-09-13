82024f41227ba8c9ad3cc67adf29808d3188e20d
MoamenElgraihy <moamenelgraihy@gmail.com>
2024-09-14 01:11:56 +0300

    Add Home Page

--- Clinic System.PLL/wwwroot/js/site.js
+++ Clinic System.PLL/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <!--jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!--Bootstrap Bundle with Popper-- >
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
