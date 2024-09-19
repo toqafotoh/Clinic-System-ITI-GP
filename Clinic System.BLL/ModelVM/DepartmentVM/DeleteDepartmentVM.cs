25fcc8285d1303545d3e1efdc091daf4dd89630c
Haneen-Elagamy <haneenelagamy21@gmail.com>
2024-09-19 03:30:04 +0300

    DeleteModelVM

--- Clinic System.BLL/ModelVM/DepartmentVM/DeleteDepartmentVM.cs
+++ Clinic System.BLL/ModelVM/DepartmentVM/DeleteDepartmentVM.cs
@@ -12,5 +12,6 @@ namespace Clinic_System.BLL.ModelVM.DepartmentVM
         public int ID { get; set; }
         public string Name { get; set; }
         public bool IsDeleted { get; set; } = false;
+        public string? PhoneNumber { get; set; }
     }
 }
---
+++ Clinic System.BLL/ModelVM/DoctorVM/DeleteDoctorVM.cs
@@ -0,0 +1,27 @@
+﻿using Clinic_System.DAL.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic_System.BLL.ModelVM.DoctorVM
+{
+    public class DeleteDoctorVM
+    {
+        public int DoctorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public int Age { get; set; } = 0;
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public Gender Gender { get; set; } = Gender.Male;
+        public string? Image { get; set; }
+        public bool? IsDeleted { get; set; } = false;
+        public Shift Shift { get; set; }
+        public int SessionPrice { get; set; }
+        public string Description { get; set; }
+        public int DeptID { get; set; }
+    }
+}
