a5c08f7994742b534d4399c72700b789c6f71759
toqafotoh <100733611+toqafotoh@users.noreply.github.com>
2024-09-20 23:31:54 +0300

    update admin view

---
+++ Clinic System.PLL/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clinic_System.PLL.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
